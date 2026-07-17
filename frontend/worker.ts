1f2d340daf6b449d3516c828e2907cc1c047c504
Sean Pearce <seanpearce9722@gmail.com>
2026-07-17 11:48:58 -0500

    Deploy React portfolio with Cloudflare Worker

---
+++ frontend/worker.ts
@@ -0,0 +1,16 @@
+/// <reference types="@cloudflare/workers-types" />
+
+interface Env {
+  ASSETS: Fetcher;
+}
+
+export default {
+  async fetch(
+    request: Request,
+    env: Env
+  ): Promise<Response> {
+
+    return env.ASSETS.fetch(request);
+
+  },
+};
